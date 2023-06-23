8b18fa459c4d23c0fd9fa87c87c5ca6cd8ef998a
nk <kumarnidhish22@gmail.com>
2023-06-23 21:17:29 +0530

    starting the api part

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ st.title("Welcome, please enter the url")
 
 URL = st.text_input("url", label_visibility = "hidden")
 
-if st.button("Get Data"):
+col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+
+if col4.button("Get Data"):
     if URL:
         d = scraper(URL)
         print(d)
